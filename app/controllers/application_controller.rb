4865c221a45049c37751296dfd37671f347cfde4
mentalveritas <mentalveritas@gmail.com>
2016-07-16 18:52:39 +0900

    Add login status&logout function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   # protect_from_forgery with: :exception
 end
+
+def login_check
+  @current_user = nil
+  if !session[:user_id].nil?
+    @current_user = User.find(session[:user_id])
+  else
+    redirect_to "/users/login"
+  end
+end
